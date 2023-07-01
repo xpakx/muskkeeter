126a0bfe52f108a24e91529eeb887140a6296dc6
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-01 21:01:12 +0200

    feat(frontend/ui): style action counts for tweet

--- frontend/src/components/Tweet.svelte
+++ frontend/src/components/Tweet.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import type { TweetDetails } from "../types/tweet";
-
+    import Fa from "svelte-fa/src/fa.svelte";
+    import {
+        faHeart,
+        faMessage,
+        faQuoteRight,
+        faReply,
+        faReplyAll,
+    } from "@fortawesome/free-solid-svg-icons";
     export let tweet: TweetDetails;
-
 
     const formatter = new Intl.DateTimeFormat("default", {
         month: "short",
@@ -17,16 +23,16 @@
     <div class="tweet-column">
         <div class="header">
             {#if tweet.retweeted}
-            <div class="retweet-header">retweeted</div>
+                <div class="retweet-header"><Fa icon={faReply} /> retweeted</div>
             {/if}
             <div class="user-header">
                 <div class="user-data">
                     <div class="user-data_name">{tweet.author.name}</div>
                     <div class="user-data_handle">@{tweet.author.username}</div>
                 </div>
-                <div class="date">{tweet.date}</div>
+                <div class="date-header">{tweet.date}</div>
             </div>
-            </div>
+        </div>
         <div class="content">
             {tweet.text}
         </div>
@@ -35,10 +41,10 @@
             {tweet.date}
         </div>
         <div class="actions">
-            <span>{tweet.replies}</span>
-            <span>{tweet.retweets}</span>
-            <span>{tweet.quotes}</span>
-            <span>{tweet.favs}</span>
+            <span><Fa icon={faMessage} /> {tweet.replies}</span>
+            <span><Fa icon={faReplyAll} /> {tweet.retweets}</span>
+            <span><Fa icon={faQuoteRight} /> {tweet.quotes}</span>
+            <span><Fa icon={faHeart} /> {tweet.favs}</span>
         </div>
     </div>
 </div>
@@ -58,8 +64,12 @@
         flex-grow: 1;
     }
 
-    .header, .content {
+    .header {
         margin-bottom: 10px;
+    }
+
+    .content {
+        margin-bottom: 5px;
     }
 
     .user-header {
@@ -74,18 +84,22 @@
 
     .user-data_name {
         font-weight: bold;
-        color: #a6adc8; 
+        color: #a6adc8;
     }
-    
+
     .user-data_handle {
         color: #f2cdcd;
     }
 
-    .date, .actions {
+    .actions {
         font-size: 13px;
+        font-weight: bold;
+        display: flex;
+        gap: 5px;
     }
 
-    .actions {
-        font-weight: bold;
+    .date {
+        font-size: 13px;
+        margin-bottom: 5px;
     }
 </style>
