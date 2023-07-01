36516b0087ab6712f4347653b26ea08cbea6f741
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-01 18:42:10 +0200

    feat(frontend): make main page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script type="ts">
+    import { goto } from "$app/navigation";
+
+    let name = "";
+
+
+    function login() {
+        if (name != "") {
+            goto(`/${name}`)
+        }
+    }
+</script>
+
+<div class="main-container">
+    <div class="search-bar">
+        <form class="search-form">
+            <input
+                type="text"
+                placeholder="Enter username..."
+                id="username"
+                bind:value={name}
+            />
+            <button type="submit" on:click={login}>go</button>
+        </form>
+    </div>
+</div>
+
+<style>
+    .main-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    .search-bar {
+        margin: 0;
+    }
+
+    form.search-form {
+        padding: 12px;
+        border-radius: 4px;
+        display: flex;
+        background: #181825;
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+        margin: auto;
+        margin-top: auto;
+        margin-top: -50px;
+    }
+
+    form.search-form input {
+        font-size: 16px;
+        width: 100%;
+        background: #1e1e2e;
+        color: #cdd6f4;
+        border: 0;
+        border-radius: 4px;
+        padding: 4px;
+        margin-right: 8px;
+    }
+
+    form.search-form button {
+        background: #1e1e2e;
+        color: #cdd6f4;
+        border: 0;
+        border-radius: 3px;
+        cursor: pointer;
+        font-weight: bold;
+    }
+</style>
