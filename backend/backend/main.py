3e999c94c1936249b864c61e0c254dd019c88b75
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-01 18:51:09 +0200

    chore(backend): add cors

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from .downloader import get_tweets
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/profile/{username}")
