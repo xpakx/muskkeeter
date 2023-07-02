770cbef4e50780b720bbfec597ab7179fdf13a36
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-02 15:35:43 +0200

    feat(frontend): let user view timeline with replies

--- frontend/src/routes/[name]/+page.svelte
+++ frontend/src/routes/[name]/+page.svelte
@@ -6,9 +6,12 @@
     let apiUri = "http://localhost:8000";
     let message: String;
     let tweets: TweetDetails[] = [];
-    getTimer(name);
+    getTimeline(name);
+    const TIMELINE = 0;
+    const REPLIES = 1;
+    let active_card = TIMELINE;
 
-    async function getTimer(username: String) {
+    async function getTimeline(username: String) {
         try {
             let response = await fetch(`${apiUri}/profile/${username}`, {
                 method: "GET",
@@ -16,11 +19,10 @@
 
             if (response.ok) {
                 let fromEndpoint = await response.json();
-                for(let tweet of fromEndpoint) {
+                for (let tweet of fromEndpoint) {
                     tweet.date = new Date(tweet.date);
                 }
                 tweets = fromEndpoint;
-
             } else {
                 const errorBody = await response.json();
                 message = errorBody.detail;
@@ -31,6 +33,48 @@
             }
         }
     }
+
+    async function getReplies(username: String) {
+        try {
+            let response = await fetch(
+                `${apiUri}/profile/${username}/replies`,
+                {
+                    method: "GET",
+                }
+            );
+
+            if (response.ok) {
+                let fromEndpoint = await response.json();
+                for (let tweet of fromEndpoint) {
+                    tweet.date = new Date(tweet.date);
+                }
+                tweets = fromEndpoint;
+            } else {
+                const errorBody = await response.json();
+                message = errorBody.detail;
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                message = err.message;
+            }
+        }
+    }
+
+    async function loadTimeline() {
+        if (active_card == TIMELINE) {
+            return;
+        }
+        active_card = TIMELINE;
+        getTimeline(name);
+    }
+
+    async function loadReplies() {
+        if (active_card == REPLIES) {
+            return;
+        }
+        active_card = REPLIES;
+        getReplies(name);
+    }
 </script>
 
 <svelte:head>
@@ -38,8 +82,13 @@
 </svelte:head>
 
 <h1>{name}'s profile</h1>
+<div class="cards">
+    <button on:click={loadTimeline}>Tweets</button>
+    <button on:click={loadReplies}>Tweets & replies</button>
+</div>
+
 {#each tweets as tweet}
-   <Tweet {tweet} /> 
+    <Tweet {tweet} />
 {/each}
 
 <style>
