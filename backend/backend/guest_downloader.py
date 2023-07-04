d54f1b74a3b7608b6fd3b4465475d1c230005ecb
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-04 12:39:41 +0200

    chore(backend): clean urls a little

--- backend/backend/guest_downloader.py
+++ backend/backend/guest_downloader.py
@@ -5,8 +5,67 @@ from datetime import datetime
 android_api_key = ("CjulERsDeqhhjSme66ECg", "IQWdVyqFxghAtURHGeGiWAsmCAGmdW3WmbEx6Hck")
 get_token_endpoint = "https://api.twitter.com/oauth2/token?grant_type=client_credentials"
 activate_token_endpoint = "https://api.twitter.com/1.1/guest/activate.json"
-user_to_id_endpoint = "https://api.twitter.com/graphql/oUZZZ8Oddwxs8Cd3iW3UEA/UserByScreenName?variables=%7B%22screen_name%22%3A%22{}%22%2C%22withSafetyModeUserFields%22%3Atrue%7D&features=%7B%22hidden_profile_likes_enabled%22%3Afalse%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22subscriptions_verification_info_verified_since_enabled%22%3Atrue%2C%22highlights_tweets_tab_ui_enabled%22%3Atrue%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%7D" 
-timeline_endpoint = "https://api.twitter.com/graphql/pNl8WjKAvaegIoVH--FuoQ/UserTweetsAndReplies?variables=%7B%22userId%22%3A%22{}%22,%22count%22%3A40,%22includePromotedContent%22%3Atrue,%22withCommunity%22%3Atrue,%22withSuperFollowsUserFields%22%3Atrue,%22withDownvotePerspective%22%3Afalse,%22withReactionsMetadata%22%3Afalse,%22withReactionsPerspective%22%3Afalse,%22withSuperFollowsTweetFields%22%3Atrue,%22withVoice%22%3Atrue,%22withV2Timeline%22%3Atrue%7D&features=%7B%22responsive_web_twitter_blue_verified_badge_is_enabled%22%3Atrue,%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue,%22verified_phone_label_enabled%22%3Afalse,%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue,%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse,%22tweetypie_unmention_optimization_enabled%22%3Atrue,%22vibe_api_enabled%22%3Atrue,%22responsive_web_edit_tweet_api_enabled%22%3Atrue,%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue,%22view_counts_everywhere_api_enabled%22%3Atrue,%22longform_notetweets_consumption_enabled%22%3Atrue,%22tweet_awards_web_tipping_enabled%22%3Afalse,%22freedom_of_speech_not_reach_fetch_enabled%22%3Afalse,%22standardized_nudges_misinfo%22%3Atrue,%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Afalse,%22interactive_text_enabled%22%3Atrue,%22responsive_web_text_conversations_enabled%22%3Afalse,%22longform_notetweets_richtext_consumption_enabled%22%3Afalse,%22responsive_web_enhance_cards_enabled%22%3Afalse%7D"
+user_to_id_endpoint = "https://api.twitter.com/graphql/oUZZZ8Oddwxs8Cd3iW3UEA/UserByScreenName"
+timeline_endpoint = "https://api.twitter.com/graphql/pNl8WjKAvaegIoVH--FuoQ/UserTweetsAndReplies"
+
+
+def get_user_to_id_params(name: str):
+    return {
+            "variables": json.dumps({
+                "screen_name": name,
+                "withSafetyModeUserFields": True
+                }),
+
+            "features": json.dumps({
+                "hidden_profile_likes_enabled": False,
+                "responsive_web_graphql_exclude_directive_enabled":  True,
+                "verified_phone_label_enabled": False,
+                "subscriptions_verification_info_verified_since_enabled": True,
+                "highlights_tweets_tab_ui_enabled": True,
+                "creator_subscriptions_tweet_preview_api_enabled": True,
+                "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
+                "responsive_web_graphql_timeline_navigation_enabled": True
+                })
+            }
+
+
+def get_timeline_endpoint_params(id: int):
+    return {
+            "variables": json.dumps({
+                "userId": id,
+                "count": 40,
+                "includePromotedContent": True,
+                "withCommunity": True,
+                "withSuperFollowsUserFields": True,
+                "withDownvotePerspective": False,
+                "withReactionsMetadata": False,
+                "withReactionsPerspective": False,
+                "withSuperFollowsTweetFields": True,
+                "withVoice": True,
+                "withV2Timeline": True
+                }),
+            "features": json.dumps({
+                "responsive_web_twitter_blue_verified_badge_is_enabled": True,
+                "responsive_web_graphql_exclude_directive_enabled": True,
+                "verified_phone_label_enabled": False,
+                "responsive_web_graphql_timeline_navigation_enabled": True,
+                "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
+                "tweetypie_unmention_optimization_enabled": True,
+                "vibe_api_enabled": True,
+                "responsive_web_edit_tweet_api_enabled": True,
+                "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
+                "view_counts_everywhere_api_enabled": True,
+                "longform_notetweets_consumption_enabled": True,
+                "tweet_awards_web_tipping_enabled": False,
+                "freedom_of_speech_not_reach_fetch_enabled": False,
+                "standardized_nudges_misinfo": True,
+                "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": False,
+                "interactive_text_enabled": True,
+                "responsive_web_text_conversations_enabled": False,
+                "longform_notetweets_richtext_consumption_enabled": False,
+                "responsive_web_enhance_cards_enabled": False
+                })
+            }
 
 
 def get_tweets(name: str):
@@ -29,8 +88,9 @@ def get_tweets(name: str):
                 "Referer": "https://twitter.com/"
                 }
         user_id_response = requests.get(
-                user_to_id_endpoint.format(name),
-                headers=headers
+                user_to_id_endpoint,
+                headers=headers,
+                params=get_user_to_id_params(name)
                 )
         if (user_id_response.status_code != 200):
             print("couldn't get user id")
@@ -39,7 +99,8 @@ def get_tweets(name: str):
         user_id = json.loads(user_id_response.content)['data']['user']['result']['rest_id']
         timeline_response = requests.get(
                 timeline_endpoint.format(user_id),
-                headers=headers
+                headers=headers,
+                params=get_timeline_endpoint_params(user_id)
                 )
         if (timeline_response.status_code != 200):
             print("couldn't get content")
