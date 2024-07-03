6cfdd35c34fdf8f5ff32c3dcbe7b8007c6dab1b1
xpakx <55586470+xpakx@users.noreply.github.com>
2024-07-03 14:36:44 +0200

    chore: clean up unused and outdated code

--- backend/backend/downloader.py
+++ backend/backend/downloader.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 headers = {
     "Host": "syndication.twitter.com",
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-GB,en;q=0.5",
     "Accept-Encoding": "gzip, deflate, br",
